99592529512394736ce21bfb933adcdac936405a
Ahmed <94145850+iamklevy@users.noreply.github.com>
2023-09-26 16:20:22 +0300

    Add files via upload

---
+++ RTOS Stack/RTOS/TMU_Interface.h
@@ -0,0 +1,13 @@
+/*
+ * TMU_Interface.h
+ *
+ *  Created on: Aug 30, 2023
+ *      Author: F1
+ */
+
+#ifndef RTOS_STACK_RTOS_TMU_INTERFACE_H_
+#define RTOS_STACK_RTOS_TMU_INTERFACE_H_
+
+
+
+#endif /* RTOS_STACK_RTOS_TMU_INTERFACE_H_ */
---
+++ RTOS Stack/RTOS/TMU_private.h
@@ -0,0 +1,13 @@
+/*
+ * TMU_private.h
+ *
+ *  Created on: Aug 30, 2023
+ *      Author: F1
+ */
+
+#ifndef RTOS_STACK_RTOS_TMU_PRIVATE_H_
+#define RTOS_STACK_RTOS_TMU_PRIVATE_H_
+
+
+
+#endif /* RTOS_STACK_RTOS_TMU_PRIVATE_H_ */
